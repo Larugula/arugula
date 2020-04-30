056e1fd37819e5e5465723df51f71c099acaee52
Brent Zhao <brentgryffindor@outlook.com>
2020-04-30 02:40:46 -0700

    added deltasetmap, unit tested insert, interator and get_delta

---
+++ include/TypeZoo/DeltaSetMap.hpp
@@ -0,0 +1,79 @@
+#ifndef DELTASETMAP_H
+#define DELTASETMAP_H
+
+#include "DeltaSet.hpp"
+#include <map>
+#include "lattice_core.hpp"
+#include "merges/map_mrg.hpp"
+
+template <typename k_type, typename set_v_type, class Func>
+class DeltaSetMap {
+public:
+
+	//type alias
+	using iterator = typename std::map<k_type, Lattice<DeltaSet<set_v_type>, Func>>::iterator;
+	using key_type = k_type;
+	using value_type = std::pair<k_type, Lattice<DeltaSet<set_v_type>, Func>>;
+	using size_type = std::size_t;
+	using delta_type = Lattice<DeltaSetMap<k_type, set_v_type, Func>, MapUnion>;
+
+	DeltaSetMap() : _base(std::map<k_type, Lattice<DeltaSet<set_v_type>, Func>>{}) {};
+
+	//copy constructor
+	DeltaSetMap(const std::map<k_type, Lattice<DeltaSet<set_v_type>, Func>>& base) : _base(base) {};
+
+	
+	iterator begin() {
+		return _base.begin();
+	}
+
+	iterator end() {
+		return _base.end();
+	}
+
+	size_type size() {
+		return _base.size();
+	}
+
+	std::pair<iterator, bool> insert(const value_type& value) {
+		return _base.insert(value);
+	}
+
+	std::pair<iterator, bool> insert(const value_type&& value) {
+		return _base.insert(value);
+	}
+
+	iterator erase(iterator pos) {
+		return _base.erase(pos);
+	}
+
+	template<class... Args>
+	std::pair<iterator, bool> emplace(Args&&... args) {
+		return _base.emplace(args...);
+	}
+
+	bool operator==(const DeltaSetMap<k_type, set_v_type, Func>& right) const {
+		return _base == right._base;
+	}
+
+	delta_type get_delta() {
+		DeltaSetMap<k_type, set_v_type, Func> delta;
+		
+		for (auto& [key, value] : _base) {
+			delta.insert(std::make_pair(key, value.get_delta()));
+		}
+
+		return Lattice(delta, MapUnion{});
+	}
+
+
+
+
+
+
+
+private:
+	std::map<k_type, Lattice<DeltaSet<set_v_type>, Func>> _base;
+};
+
+#endif // DELTASETMAP_H
---
+++ test/smoke_type/deltasetmap_test.cpp
@@ -0,0 +1,40 @@
+#include "../catch.hpp"
+#include "lattice_core.hpp"
+#include "TypeZoo/DeltaSet.hpp"
+#include "TypeZoo/DeltaSetMap.hpp"
+#include "merges/setop_mrg.hpp"
+#include <iostream>
+
+
+TEST_CASE("basic tests") {
+	std::map<std::string, Lattice<DeltaSet<int>, Union>> left = { {"xx", Lattice(DeltaSet<int>(std::set<int>{1,2,3}), Union{})},
+																  {"yy", Lattice(DeltaSet<int>(std::set<int>{1,20,30}), Union{})} };
+	std::map<std::string, Lattice<DeltaSet<int>, Union>> result;
+	DeltaSetMap leftmap(left);
+	DeltaSetMap resultmap(result);
+
+	for (auto& e : leftmap) {
+		resultmap.insert(e);
+	}
+
+	REQUIRE(leftmap == resultmap);
+}
+
+TEST_CASE("delta test") {
+	DeltaSet<int> t1(std::set<int>{1, 2, 3});
+	t1.merge(DeltaSet<int>(std::set<int>{1, 20, 30}));
+	DeltaSet<int> t2(std::set<int>{5, 6, 7});
+	t2.merge(DeltaSet<int>(std::set<int>{5, 60, 70}));
+
+	std::map<std::string, Lattice<DeltaSet<int>, Union>> left = { {"xx", Lattice(t1, Union{})},
+																  {"yy", Lattice(t2, Union{})}};
+	std::map<std::string, Lattice<DeltaSet<int>, Union>> expected = { {"xx", Lattice(DeltaSet<int>(std::set<int>{20,30}), Union{})},
+																    {"yy", Lattice(DeltaSet<int>(std::set<int>{60,70}), Union{})} };
+	DeltaSetMap leftmap(left);
+	DeltaSetMap expectedmap(expected);
+	Lattice expectedLattice(expectedmap, MapUnion{});
+
+	DeltaSetMap<std::string, int, Union>::delta_type resultLattice = leftmap.get_delta();
+
+	REQUIRE(resultLattice == expectedLattice);
+}
